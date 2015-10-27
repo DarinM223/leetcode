550b51ade2c867ae3ad5ed57d2f5deb8988fe5a7
Darin Minamoto <darinm223@gmail.com>
2015-10-26 23:37:48 -0700

    Added Palindrome Number solution

---
+++ palindrome_number.py
@@ -0,0 +1,32 @@
+import math
+
+class Solution(object):
+    """
+    Solution reverses the integer, then checks if the reversed integer
+    equals the original integer. If it does, it's a palindrome
+    """
+    
+    def reverseInteger(self, n):
+        """
+        Reverses a number and returns the reversed number
+        """
+        result = 0
+        while n != 0:
+            digit = n % 10
+            result += digit
+            n /= 10
+            if n != 0:
+                result *= 10
+                
+        return result
+    
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
+        # for some reason negative numbers don't count
+        if x < 0:
+            return False
+        return self.reverseInteger(abs(x)) == abs(x)
